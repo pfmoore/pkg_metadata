7d1d1a1384319c535b90da11fc2fce3f7f6fea4d
Paul Moore <p.f.moore@gmail.com>
2022-05-09 15:12:39 +0100

    Bump version

--- src/pkg_metadata/__init__.py
+++ src/pkg_metadata/__init__.py
@@ -1,6 +1,6 @@
 """Utility classes for handling packaging metadata.
 """
 
-__version__ = "0.1"
+__version__ = "0.2"
 
 from .metadata import bytes_to_json, msg_to_json, json_to_bytes, pyproject_to_json
